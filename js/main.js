cf85df20115f9b539cb7594f06746a4a17472b63
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2022-09-11 17:33:55 -0300

    nenu js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function(){
+    var $body = document.querySelector('body');
+    $body.classList.remove('no-js')
+    $body.classList.add('js')
+
+    var menu = new Menu({
+        container: '.header-nav',
+        toggleBtn: '.header-btnMenu',
+        widthEnabled: 1024
+    })
+})()
